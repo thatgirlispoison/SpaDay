e57b550ca428ca1d6a635a516e91b5f66f7269a9
Kelly Thomas <kellydye77@gmail.com>
2021-02-01 14:16:28 -0600

    Finished studio

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -12,7 +12,7 @@ namespace SpaDay.Controllers
     {
         public bool CheckSkinType(string skinType, string facialType)
         {
-
+            //Add the client’s skintype and the appropriateFacials list as a ViewBag property.
             if (facialType != "Microdermabrasion")
             {
                 if (skinType == "oily" && facialType != "Rejuvenating")
@@ -42,6 +42,7 @@ namespace SpaDay.Controllers
         [Route("/spa")]
         public IActionResult Menu(string name, string skintype, string manipedi)
         {
+            ViewBag.name = name;
             List<string> facials = new List<string>()
             {
                 "Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"
@@ -55,6 +56,9 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+            ViewBag.skintype = skintype;
+            ViewBag.appropriateFacials = appropriateFacials;
+            ViewBag.manipedi = manipedi;
             return View();
         }
 
